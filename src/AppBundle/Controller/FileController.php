93dab2901f5dfbac940b19cae69f46b8b37324b8
Michał <Wozniak.michal12@gmail.com>
2018-09-30 21:15:14 +0200

    Modyfikacje
    - dodanie popup'u szczegółów
    - poprawienie wyglądu (przeniesienie menu)
    - poprawki do przejrzystości kodu (backend)

--- src/AppBundle/Controller/FileController.php
+++ src/AppBundle/Controller/FileController.php
@@ -11,7 +11,10 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\UploadFile;
 use AppBundle\Form\UploadFileForm;
 use AppBundle\Service\FileUploader;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ConnectionException;
 use Doctrine\DBAL\DBALException;
+use League\Csv\CharsetConverter;
 use League\Csv\Exception;
 use League\Csv\Reader;
 use League\Csv\Statement;
@@ -24,82 +27,139 @@ use AppBundle\Service\CsvFileValidator;
 
 class FileController extends Controller
 {
+
+    private $dbConnection;
+    private $uploadFileModel;
+    private $csvFileValidator;
+
+    private $csvFileHeaders = array();
+    private $csvReader;
+
+    public function __construct(
+        Connection $connection,
+        UploadFile $uploadFileModel,
+        CsvFileValidator $csvFileValidator
+    )
+    {
+        $this->dbConnection = $connection;
+        $this->uploadFileModel = $uploadFileModel;
+        $this->csvFileValidator = $csvFileValidator;
+    }
+
     /**
      * @Route("/file/", name="_file_index")
      */
-    public function indexController(Request $request = null, FileUploader $fileUploader) {
+    public function indexController(Request $request = null, FileUploader $fileUploader)
+    {
 
-        $upload = new UploadFile();
-        $validator = new CsvFileValidator();
-
-        $db = $this->get('database_connection');
-
-        $form = $this->createForm(UploadFileForm::class, $upload);
+        $form = $this->createForm(UploadFileForm::class, $this->uploadFileModel);
         $form->handleRequest($request);
 
+        //TODO: Wyrzucić do to serwisu
+
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $file = $upload->getCsv();
-            $filename = $fileUploader->upload($file);
-            $upload->setCsv($filename);
-
-            //Add to database rows from csv
-            $csv = Reader::createFromPath('uploads/' . $filename, 'r');
-            $csv->setDelimiter(';');
-
-            $records = (new Statement())->process($csv);
-
-            //Get headers
-            $header = $validator->getFileHeaders($records);
-
-            if (!empty($header)) {
-
-                $db->beginTransaction();
-                $upperlimit = array_search('UID', $header);
-                $alldata = (count($header) - intval($upperlimit) - 1);
-                $column_list = $validator->getQueryColumnList($alldata);
-
-                try {
-                    $i = 1;
-                    $db->executeQuery('DELETE FROM imported_data');
-                    $db->executeQuery('DELETE FROM column_name_schema');
-                    $query = $db->prepare('INSERT INTO column_name_schema ('. ltrim($column_list["cnames"], ',') .') VALUES ('. ltrim($column_list["values"], ',') .')');
-
-                    foreach ($header as $hKey => $hr) {
-                        if ($hKey <= $upperlimit or trim($hr) == 'Uwagi' or empty($hr)) { continue; }
-                        $query->bindValue($i++, $hr);
-                    }
-
-                    $query->execute();
-
-                    foreach ($csv->getRecords() as $rKey => $csvRow) {
-
-                        if ($rKey < 2) { continue; }
-
-                        $query = $db->prepare('INSERT INTO imported_data (lab, case_number, case_role, sample_number, uid'. $column_list["cnames"] .', comments) VALUES (?, ?, ?, ?, ?'.$column_list["values"].', ?)');
-
-                        foreach ($csvRow as $crKey => $cr) {
-                            $query->bindValue(($crKey+1), str_replace(',', '.', $cr));
-                        }
-                        $query->execute();
-                    }
-                } catch(DBALException $e) {
-                    $db->rollBack();
-                    throw new DBALException($e->getMessage());
-                }
-
-                $this->addFlash('success', 'Data from the uploaded file has been correctly added to the database.');
-                $db->commit();
-
-            } else {
-                $this->addFlash('error', 'Error: Headers failed to be specified.');
-            }
-
-            return $this->redirect($this->generateUrl('_file_index'));
+            $this->prepareUploadedFile($fileUploader);
         }
 
-        return $this->render('file/uploadForm.html.twig', [
+        return $this->render('file/uploadForm.html.twig',
+            [
             'form' => $form->createView()
-        ]);
+            ]
+        );
+    }
+
+    private function prepareUploadedFile(FileUploader $fileUploader)
+    {
+        $filename = $fileUploader->upload($this->uploadFileModel->getCsv());
+
+        $this->uploadFileModel->setCsv($filename);
+
+        try {
+
+            $this->csvReader = Reader::createFromPath('uploads/'.$filename, 'r');
+            $this->csvReader->setDelimiter(';');
+
+            $processedRecords = (new Statement())->process($this->csvReader);
+
+            $this->csvFileHeaders = $this->csvFileValidator->getFileHeaders($processedRecords);
+
+            if (!empty($this->csvFileHeaders)) {
+                $this->createColumnNameSchema();
+            } else {
+                $this->addFlash('error', 'Błąd: Aplikacja nie była w stanie wskazać nagłówków kolumn przesłanego pliku. Sprawdź czy plik posiada prawidłowo opisane kolumny.');
+            }
+
+        } catch (\League\Csv\Exception $exception) {
+            throw new \League\Csv\Exception('Error: ' . $exception->getMessage());
+        }
+
+        return $this->redirect($this->generateUrl('_file_index'));
+    }
+
+    private function createColumnNameSchema()
+    {
+        $this->dbConnection->beginTransaction();
+
+        try {
+
+            $this->dbConnection->exec('DELETE FROM column_name_schema');
+
+            $columnSchemaInsert = $this->csvFileValidator->prepareColumnSchemaData();
+
+            $this->dbConnection->insert('column_name_schema', $columnSchemaInsert );
+
+            $this->dbConnection->commit();
+
+        } catch(DBALException $exception) {
+            $this->dbConnection->rollBack();
+            throw new DBALException('Error: ' . $exception->getMessage());
+        }
+
+        $this->insertIntoDatabase();
+    }
+
+    private function insertIntoDatabase()
+    {
+        $followRecordKey = 0;
+        $this->dbConnection->beginTransaction();
+
+        try {
+
+            $this->dbConnection->exec('DELETE FROM imported_data');
+            $this->dbConnection->exec('DELETE FROM compare_all_to_all');
+            $this->dbConnection->exec('DELETE FROM compare_m_p_d');
+
+
+            foreach ($this->csvReader->getRecords() as $recordKey => $csvRow) {
+
+                $followRecordKey = $recordKey;
+
+                if ($this->csvFileValidator->isTooManyEmptyRows()) {
+                    break;
+                }
+
+                if ($this->csvFileValidator->isRowEmpty($csvRow)) {
+                    continue;
+                }
+
+                if (!$this->csvFileValidator->rowIsHeader($csvRow)) {
+
+                    $importedDataColumnNames = $this->csvFileValidator->prepareImportedDataColumns();
+
+                    $preparedCsvData = $this->csvFileValidator->modifyRowCharsetToUtf8($csvRow);
+
+                    $this->dbConnection->insert('imported_data', array_combine($importedDataColumnNames, $preparedCsvData) );
+                }
+            }
+
+            $this->dbConnection->commit();
+
+            $this->addFlash('success', 'Dane zostały prawidłowo zaimportowane do bazy.');
+
+        } catch (DBALException $exception) {
+            $this->addFlash('error', 'Błąd: Błędne wartości w rekordzie ('.++$followRecordKey.').');
+        }
+
+        $this->redirect($this->generateUrl('_file_index'));
     }
 }
--- src/AppBundle/Service/CsvFileValidator.php
+++ src/AppBundle/Service/CsvFileValidator.php
@@ -13,46 +13,141 @@ use League\Csv\Statement;
 
 class CsvFileValidator
 {
+    const COLUMN_NAME_OF_PERMANENT_START = 'UID';
+    const COLUMN_NAME_OF_PERMANENT_LIMIT = 'Uwagi';
+
+    const MAIN_COLUMN_NAMES = array(
+        array (
+            'inFile' => 'LAB',
+            'inDB' => 'lab'
+        ),
+        array (
+            'inFile' => 'Nr sprawy',
+            'inDB' => 'case_number'
+        ),
+        array (
+            'inFile' => 'Rola w sprawie',
+            'inDB' => 'case_role'
+        ),
+        array (
+            'inFile' => 'Nr próbki',
+            'inDB' => 'sample_number'
+        ),
+        array (
+            'inFile' => 'UID',
+            'inDB' => 'UID'
+        )
+    );
+
+    private $csvFileHeadersRow = array();
+    private $emptyRowCounter = 0;
+
 
     public function rowIsHeader(Array $record) : bool
     {
-        $headers = array( 'LAB', 'Nr sprawy', 'Rola w sprawie', 'Nr próbki', 'UID' );
-
-        foreach($headers as $key => $header) {
-            if (in_array($header, $record)) {
-                return true;
-            }
+        foreach( self::MAIN_COLUMN_NAMES as $key => $header ) {
+            return in_array($header['inFile'], $record);
         }
 
         return false;
     }
 
-    public function getFileHeaders(ResultSet $records)
+    public function getFileHeaders(ResultSet $records) : array
     {
         foreach ($records->getRecords() as $hkey => $headerRow) {
 
             if ($hkey == 4) { break; }
 
             if($this->rowIsHeader($headerRow)) {
+
+                $this->csvFileHeadersRow = $headerRow;
+
                 return $headerRow;
             }
         }
 
-        return null;
+        return array();
     }
 
-    public function getQueryColumnList($alldata) : array
+    public function prepareColumnSchemaData() : array
     {
-        $arr = array(
-            'cnames' => '',
-            'values' => ''
-        );
+        $i = 0;
+        $columnSchemaInsert = array();
 
-        for ($i = 1; $i < $alldata; $i++) {
-            $arr['cnames'] .= ', data_'.$i;
-            $arr['values'] .= ', ?';
+        foreach ( $this->csvFileHeadersRow as $columnName ) {
+            if ($this->shouldStopCollectData($columnName)) {
+                break;
+            }
+
+            if ($i >= 1) {
+                $columnSchemaInsert['data_'.$i] = $columnName;
+                $i++;
+            }
+
+            if ($this->shouldStartCollectData($columnName)) {
+                $i++;
+            }
         }
 
-        return $arr;
+        return $columnSchemaInsert;
+    }
+
+    private function shouldStartCollectData($columnName) : bool
+    {
+        return $columnName == CsvFileValidator::COLUMN_NAME_OF_PERMANENT_START;
+    }
+
+    private function shouldStopCollectData($columnName) : bool
+    {
+        return $columnName == CsvFileValidator::COLUMN_NAME_OF_PERMANENT_LIMIT;
+    }
+
+    public function prepareImportedDataColumns() : array
+    {
+        $preparedImportData = array();
+
+        $namesOfAllelColumnsToFill = $this->prepareColumnSchemaData();
+
+        foreach(self::MAIN_COLUMN_NAMES as $key => $columName) {
+            $preparedImportData[] = $columName['inDB'];
+        }
+
+        foreach($namesOfAllelColumnsToFill as $schemaColumnName => $allelColumnName) {
+            $preparedImportData[] = $schemaColumnName ;
+        }
+
+        $preparedImportData[] = 'comments';
+
+        return $preparedImportData;
+    }
+
+    public function isRowEmpty($csvRow) : bool
+    {
+        $localRecordEmptyCounter = 0;
+
+        foreach ($csvRow as $key => $value) {
+            if ($localRecordEmptyCounter >= 3) {
+                $this->emptyRowCounter++;
+                return true;
+            }
+
+            if (empty($value)) {
+                $localRecordEmptyCounter++;
+            }
+        }
+
+        return false;
+    }
+
+    public function isTooManyEmptyRows()
+    {
+        return ( $this->emptyRowCounter >= 3 ? true : false );
+    }
+
+    public function modifyRowCharsetToUtf8($csvRow)
+    {
+        return array_map(function($n){
+                    return iconv('windows-1252', 'UTF-8', str_replace(',', '.', $n));
+                }, $csvRow);
     }
 }
